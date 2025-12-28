e7fadf3a1d1e55fb9d33ae9a7525ddedf28d0b46
AllPoland <52187288+AllPoland@users.noreply.github.com>
2025-12-27 18:07:23 -0700

    hello world !
    good
    
    morning
    
    it's afternoon
    
    you're doing the
    
    same shit

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace UNBEATAP;
+
+[BepInPlugin(PluginReleaseInfo.PLUGIN_GUID, PluginReleaseInfo.PLUGIN_NAME, PluginReleaseInfo.PLUGIN_VERSION)]
+[BepInProcess("UNBEATABLE.exe")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    public static float MaxScheduleOffset => RhythmConsts.LeniencyMilliseconds + 20f;
+    public static float MinScheduleOffset = 20f;
+
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+
+        Logger.LogInfo($"Plugin {PluginReleaseInfo.PLUGIN_GUID} is loaded!");
+    }
+}
---
+++ PluginReleaseInfo.cs
@@ -0,0 +1,8 @@
+namespace UNBEATAP;
+
+public class PluginReleaseInfo
+{
+    public const string PLUGIN_GUID = "unbeatAP";
+    public const string PLUGIN_NAME = "unbeatAP";
+    public const string PLUGIN_VERSION = "0.1.0";
+}
