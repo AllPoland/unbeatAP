36f2f39477e21f437d4e71621fd1b5f6cb1ef70a
AllPoland <52187288+AllPoland@users.noreply.github.com>
2026-01-18 18:26:24 -0700

    bump version number
    keeping compatibility with 0.1.0

--- PluginReleaseInfo.cs
+++ PluginReleaseInfo.cs
@@ -6,7 +6,7 @@ public class PluginReleaseInfo
 {
     public const string PLUGIN_GUID = "unbeatAP";
     public const string PLUGIN_NAME = "unbeatAP";
-    public const string PLUGIN_VERSION = "0.1.0";
+    public const string PLUGIN_VERSION = "0.1.1";
 
     public static readonly Version[] COMPATIBLE_VERSIONS =
     [
