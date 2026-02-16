a8876cf0858ce776559c5de7717cf7562bbc324d
AllPoland <52187288+AllPoland@users.noreply.github.com>
2026-02-15 19:28:09 -0700

    bump version number

--- PluginReleaseInfo.cs
+++ PluginReleaseInfo.cs
@@ -6,10 +6,10 @@ public class PluginReleaseInfo
 {
     public const string PLUGIN_GUID = "unbeatAP";
     public const string PLUGIN_NAME = "unbeatAP";
-    public const string PLUGIN_VERSION = "0.1.3";
+    public const string PLUGIN_VERSION = "0.2.0";
 
     public static readonly Version[] COMPATIBLE_VERSIONS =
     [
-        new Version(0, 1, 3)
+        new Version(0, 2, 0)
     ];
 }
