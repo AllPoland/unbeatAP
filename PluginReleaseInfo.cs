eb5872f1e1148236d67973221f716f373cd92d89
AllPoland <52187288+AllPoland@users.noreply.github.com>
2026-03-22 13:23:12 -0600

    bump version to 0.3
    while it's techinically backwards compatible with 0.2 apworlds, the apworld won't be

--- PluginReleaseInfo.cs
+++ PluginReleaseInfo.cs
@@ -6,7 +6,7 @@ public class PluginReleaseInfo
 {
     public const string PLUGIN_GUID = "unbeatAP";
     public const string PLUGIN_NAME = "unbeatAP";
-    public const string PLUGIN_VERSION = "0.2.2";
+    public const string PLUGIN_VERSION = "0.3.0";
 
     public static readonly Version[] COMPATIBLE_VERSIONS =
     [
