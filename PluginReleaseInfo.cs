015a877c060c0f55333ac5aff9e6e07b8287dcb4
Maya <251304794+estradiol-valerate@users.noreply.github.com>
2026-07-01 19:05:10 -0500

    bump version

--- PluginReleaseInfo.cs
+++ PluginReleaseInfo.cs
@@ -6,11 +6,12 @@ public class PluginReleaseInfo
 {
     public const string PLUGIN_GUID = "unbeatAP";
     public const string PLUGIN_NAME = "unbeatAP";
-    public const string PLUGIN_VERSION = "0.5.0";
+    public const string PLUGIN_VERSION = "0.6.0";
 
     public static readonly Version[] COMPATIBLE_VERSIONS =
     [
         new Version(0, 4, 0),
-        new Version(0, 5, 0)
+        new Version(0, 5, 0),
+        new Version(0, 6, 0)
     ];
 }
